66ded53aca2523cb2ddfe30816aa4feafc55cb04
chathura-madhusanka <chathura.activities@gmail.com>
2014-02-28 17:21:19 +0530

    Update BinarySearchTree.java with some comments

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,8 @@
 /*
-This class is used to handle Binary Search Tree operations. 
+This class is used to handle Binary Search Tree operations. According to the
+assignment, I put three operation methods in this class file. They are Insert,Search and 
+Delete operation. User can do search by Book title and ISBN number with overloaded method
+'search' and do delete by book title and ISBN numer with overloaded method 'remove'.
 
 Class: 	BinarySearchTree
 Constructor: Default constructor
@@ -7,6 +10,8 @@ Methods:
 	public void insert(Book book)
 	public Node search(String title)
 	public Node search(int ISBN)
+	public boolean remove(String title)
+	public boolean remove(int ISBN)
 	
 */
 
